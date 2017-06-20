5f6dab0b598d1c13e72c66bf4232193e2c9f7fe1
[Ben Goodwin] <benjamin.wayne.goodwin@gmail.com>
2017-06-20 19:02:56 -0400

    // first upload of game to GIT

---
+++ SpaceWakeUp/Assets/Scripts/changeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+	public void changeToScene(int sceneToChangeTo)
+	{
+		SceneManager.LoadScene (sceneToChangeTo);
+	}
+
+}
